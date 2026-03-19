4ca0ca30773588648437ea7640e2e889b8431a2f
Sandeepk2907 <sandeepkharwar278@gmail.com>
2026-03-19 15:10:15 +0530

    Refactor Dijkstra's algorithm implementation

--- lab4.py
+++ lab4.py
@@ -1,9 +1,13 @@
+"""Design and implement C/JAVA/Python Program to find shortest paths from a given 
+vertex in a weighted connected graph to other vertices using Dijkstra's algorithm.
+"""
+
 INF = 99999
 
 def dijkstra(graph, source, n):
     visited = [False] * n
     dist = [INF] * n
-
+  
     dist[source] = 0
 
     for _ in range(n):
